77c769a640a1834cdf5ba6eeae008559f32b6a02
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 00:42:49 +0800

    Expand country-sector CN page generation

--- data/country-product-pages.js
+++ data/country-product-pages.js
@@ -1,10 +1,10 @@
 // Programmatic SEO routes. Numeric default values are added only after validation against corrected 2026 EU data.
 export const countryProductPages=[
-{country:'china',countryName:'China',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']},
+{country:'china',countryName:'China',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7603','7604','7605','7606','7607','7608','7609','7610','7611','7612','7613','7614','7616']},
 {country:'china',countryName:'China',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
-{country:'china',countryName:'China',sector:'cement',sectorName:'Cement',examples:['2523 10 00','2523 21 00','2523 29 00']},
+{country:'china',countryName:'China',sector:'cement',sectorName:'Cement',examples:['2507 00 80 80','2523 10 00','2523 21 00','2523 29 00','2523 30 00','2523 90 00']},
 {country:'india',countryName:'India',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
-{country:'india',countryName:'India',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']},
+{country:'india',countryName:'India',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7603','7604','7605','7606','7607','7608','7609','7610','7611','7612','7613','7614','7616']},
 {country:'turkey',countryName:'Türkiye',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
-{country:'turkey',countryName:'Türkiye',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']}
+{country:'turkey',countryName:'Türkiye',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7603','7604','7605','7606','7607','7608','7609','7610','7611','7612','7613','7614','7616']}
 ];
