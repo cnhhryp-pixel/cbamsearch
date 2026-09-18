c4445ebd5a89e59e0b02988b39a7c93878135de9
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 00:19:13 +0800

    Add official CBAM price and default-value data model

---
+++ data/default-values-meta.js
@@ -0,0 +1,6 @@
+export const defaultValueSources=[
+{annex:'Annex I',title:'Imported goods except electricity',use:'Default values for embedded emissions of imported goods and certain precursors.'},
+{annex:'Annex II',title:'Indirect embedded emissions',use:'Default values for specific indirect embedded emissions where the rules call for default values.'},
+{annex:'Annex III',title:'Imported electricity',use:'Default values for embedded direct emissions in imported electricity.'},
+{annex:'Annex IV',title:'Unknown precursor country',use:'Default values used for precursors where the country of production cannot be identified.'}
+];
