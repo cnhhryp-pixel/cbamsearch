90aabe3d2378a6523e41d4914b598342fc065daf
cnhhryp-pixel <cnhhryp@gmail.com>
2026-09-19 00:20:14 +0800

    Add country-sector CBAM programmatic SEO pages

---
+++ data/country-product-pages.js
@@ -0,0 +1,10 @@
+// Programmatic SEO routes. Numeric default values are added only after validation against corrected 2026 EU data.
+export const countryProductPages=[
+{country:'china',countryName:'China',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']},
+{country:'china',countryName:'China',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
+{country:'china',countryName:'China',sector:'cement',sectorName:'Cement',examples:['2523 10 00','2523 21 00','2523 29 00']},
+{country:'india',countryName:'India',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
+{country:'india',countryName:'India',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']},
+{country:'turkey',countryName:'Türkiye',sector:'steel',sectorName:'Iron & steel',examples:['7304','7306','7308','7318']},
+{country:'turkey',countryName:'Türkiye',sector:'aluminium',sectorName:'Aluminium',examples:['7601','7604','7606','7610']}
+];
